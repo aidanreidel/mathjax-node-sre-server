b665aba55d9bb893acd28c28bece2b1fe9cb0aa2
Aidan Reidel <aidan.reidel@gmail.com>
2019-12-28 12:50:17 -0800

    Commented out failing test example and added notes

--- test/tape-and-tap-diff-example.js
+++ test/tape-and-tap-diff-example.js
@@ -2,12 +2,14 @@ const test = require('tape') // Actual test script
 
 // Simple test function to see how tape and tap-diff work together
 test('Example: timing test', function(t) {
-  t.plan(2)
+  // to see what it looks like when a test fails swap t.plan(1) to t.plan(2)
+  // And uncomment the lines below
+  t.plan(1) // t.plan(2)
 
   t.equal(typeof Date.now, 'function')
-  var start = Date.now()
 
-  setTimeout(function() {
-    t.equal(Date.now() - start, 100)
-  }, 100)
+  // var start = Date.now()
+  // setTimeout(function() {
+  //   t.equal(Date.now() - start, 100)
+  // }, 100)
 })
