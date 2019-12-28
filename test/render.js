f77c5ae6cf768a2a9e5470b47613f7f4583d3fca
Aidan Reidel <aidan.reidel@gmail.com>
2019-12-28 12:44:53 -0800

    Refactored server testing for server refactoring

--- test/render.js
+++ test/render.js
@@ -1,26 +1,13 @@
 const test = require('tape') // Actual test script
 const got = require('got') // For issuing http requests
 
-test('Hitting the render endpoint and checking for properly formated speech data', t => {
-  t.plan(2)
-
-  got('http://localhost:3000/render')
-    .then(response => {
-      let data = JSON.parse(response.body)
-      t.equal(data.speakText, 'E = mc^2')
-      t.equal(data.speech, 'upper E equals m c squared')
-    })
-    .catch(error => {
-      console.log(error)
-    })
-})
 const testBody = {
   LaTeX: 'E = mc^2'
 }
 test('Posting to the test endpoint', t => {
   t.plan(2)
   got
-    .post('http://localhost:3000/render-request', {
+    .post('http://localhost:3000/', {
       json: testBody
     })
     .json()
