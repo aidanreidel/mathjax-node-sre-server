feea2753903b9236194945423d37559b30357caa
Aidan Reidel <aidan.reidel@gmail.com>
2019-12-28 13:38:59 -0800

    Adding some logging info

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ mjAPI.config({
 mjAPI.start()
 
 app.post('/', (req, res) => {
+  console.log(
+    new Date(),
+    'POST from',
+    req.headers['x-forwarded-for']
+      ? req.headers['x-forwarded-for']
+      : req.connection.remoteAddress
+  )
   if (!req.body.LaTeX) {
     console.log('Request Body Empty')
     return res.send('Request Body Empty')
@@ -24,11 +31,13 @@ app.post('/', (req, res) => {
     },
     function(data) {
       if (!data.errors) {
-        console.log(data)
+        // console.log(data)
         console.log(data.speakText)
         if (data.speech) console.log(data.speech)
-
         res.send(data)
+      } else {
+        console.log(data.errors)
+        res.send({ errors: data.errors })
       }
     }
   )
