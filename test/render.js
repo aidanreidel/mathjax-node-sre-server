78d7c17cde2aeba211ce25c947ee7ab482f587da
Aidan Reidel <aidan.reidel@gmail.com>
2020-01-03 13:44:11 -0800

    Added some fraction tests

--- test/render.js
+++ test/render.js
@@ -21,6 +21,37 @@ test('Posting valid LaTeX to the render endpoint should return the right speech'
     })
 })
 
+const simpleFraction = {
+  LaTeX: '0.5 = \\frac{1}{2}'
+}
+
+const quadraticFormula = {
+  LaTeX: 'x = \\frac{-b \\pm \\sqrt{b^2 - 4ac}}{2a}'
+}
+
+test('Rendering fractions should produce the correct speech output', t => {
+  t.plan(2)
+  got
+    .post(api, {
+      json: simpleFraction
+    })
+    .json()
+    .then(data => {
+      t.equal(data.speech, '0.5 equals one-half')
+    })
+  got
+    .post(api, {
+      json: quadraticFormula
+    })
+    .json()
+    .then(data => {
+      t.equal(
+        data.speech,
+        'x equals StartFraction negative b plus-or-minus StartRoot b squared minus 4 a c EndRoot Over 2 a EndFraction'
+      )
+    })
+})
+
 const emptyBody = {}
 test('Posting an empty request to the render endpoint should return message that the body was empty', t => {
   t.plan(1)
