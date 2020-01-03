72b307e85d7c6cceebc8d7fd257bb75654591f25
Aidan Reidel <aidan.reidel@gmail.com>
2020-01-03 13:20:32 -0800

    Updated test to show different api locations

--- test/render.js
+++ test/render.js
@@ -1,13 +1,17 @@
 const test = require('tape') // Actual test script
 const got = require('got') // For issuing http requests
 
+// Change which of these is commented out if you are testing locally or production
+// const api = 'http://localhost:3000/'         // Local
+const api = 'https://textosvg.aidanreidel.com/' // Production
+
 const testBody = {
   LaTeX: 'E = mc^2'
 }
 test('Posting valid LaTeX to the render endpoint should return the right speech', t => {
   t.plan(2)
   got
-    .post('http://localhost:3000/', {
+    .post(api, {
       json: testBody
     })
     .json()
@@ -21,7 +25,7 @@ const emptyBody = {}
 test('Posting an empty request to the render endpoint should return message that the body was empty', t => {
   t.plan(1)
   got
-    .post('http://localhost:3000/', {
+    .post(api, {
       json: emptyBody
     })
     .then(data => {
